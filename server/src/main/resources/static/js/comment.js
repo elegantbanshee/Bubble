393a504dca84be3649acf4cd49acdfe25bff4858
Rolando Islas <elegantbansheea@gmail.com>
2022-03-28 18:48:37 -0700

    Added Disqus

---
+++ server/src/main/resources/static/js/comment.js
@@ -0,0 +1,8 @@
+var Comment = {};
+
+Comment.main = function () {
+    var textfield = document.getElementById("comment-field")
+    var submit = document.getElementById("comment-submit")
+};
+
+window.addEventListener("load", Comment.main)
